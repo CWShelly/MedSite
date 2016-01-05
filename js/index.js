5cc3c4e03c845c88791be8589d99d1f6f28e0a1b
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-01-05 15:10:40 -0800

    updated index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,55 @@
-alert('hey');
+// $(document).ready(function($){
+//   $('#checkbox').change(function(){
+//     setInterval(function(){
+//       moveR();
+//     },3000);
+//   });
+
+  $(document).ready(function($){
+    // $('#checkbox').change(function(){
+      setInterval(function(){
+        moveR();
+      },4000);
+    // });
+// });
+
+var slideX = $('#slides ul li').length;
+var slideW = $('#slides ul li').width();
+var slideH = $('#slides ul li').height();
+var slideULW = slideX * slideW;
+
+$('#slides').css({width: slideW, height: slideH});
+
+$('#slides ul').css({width:slideULW, marginLeft: -slideW});
+
+$('#slides ul li:last-child').prependTo('#slides ul');
+
+function moveL(){
+  $('#slides ul').animate({
+    left: + slideW
+  }, 200, function(){
+    $('#slides ul li:last-child').prependTo('#slides ul');
+    $('#slides ul').css('left', '');
+  });
+};
+
+
+function moveR(){
+  $('#slides ul').animate({
+    left: -slideW}, 200, function(){
+      $('#slides ul li:first-child').appendTo('#slides ul');
+      $('#slides ul').css('left', '');
+    });
+  };
+
+
+$('a.previous').click(function(){
+  moveL();
+});
+
+$('a.next').click(function(){
+  moveR();
+
+});
+
+});
