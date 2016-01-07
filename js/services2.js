e928f4399f152861300e8fc9eef43fb3177578ea
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-01-07 15:16:50 -0800

    modified services.css services.html and services2.js to include subNavbar with drop down menus for areas of surgery offered. Re-pointed services.html js file from js/services.js to js/services2.js

--- js/services2.js
+++ js/services2.js
@@ -1,1 +1,39 @@
 // alert('hey');
+
+// $(document).ready(function(){
+//   $('#services').hover(function(){
+//     $(this).find('#card').stop(true, true).slideDown()
+//   },
+//     function(){
+//       $(this).find('#card').stop(true, true).slideUp()
+//     }).find('#card').hide()
+//
+// });
+
+
+$(document).ready(function(){
+  $('#card').hover(function(){
+    $('#card').find('.subCard').stop(true, true).slideDown()
+  },
+function(){
+  $('#card').find('.subCard').stop(true, true).slideUp()}).find('.subCard').hide()
+});
+
+
+$(document).ready(function(){
+  $('#thor').hover(function(){
+    $('#thor').find('.subThor').stop(true, true).slideDown()
+  },
+function(){
+  $('#thor').find('.subThor').stop(true, true).slideUp()}).find('.subThor').hide()
+});
+
+
+
+$(document).ready(function(){
+  $('#gen').hover(function(){
+    $('#gen').find('.subGen').stop(true, true).slideDown()
+  },
+function(){
+  $('#gen').find('.subGen').stop(true, true).slideUp()}).find('.subGen').hide()
+});
